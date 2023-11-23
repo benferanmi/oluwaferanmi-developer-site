7dc42ed9a607914b733ec035646ed8a9aacbfc20
opafunso benjamin <benferanmiopafunso@gmail.com>
2023-11-23 20:03:34 +0100

    remving the vercel analytics code that i added earlier

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import IndexPage from './IndexPage.jsx'
 import Layout from './Layout.jsx'
 import Contact from './Contact/Contact'
 import Blog from './Blogs'
-import { Analytics } from '@vercel/analytics/react';
 
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
       <Route path='/' element={<Layout />} />
       <Route path='/contact' element={<Contact />} />
       <Route path='/blogs' element={<Blog />} />
-      <Analytics />
       </Routes>
     </>
   )
