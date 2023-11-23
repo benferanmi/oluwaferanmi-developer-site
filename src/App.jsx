16cc231ff4067156e38e2bfd5bb7eb3ddee99e56
opafunso benjamin <benferanmiopafunso@gmail.com>
2023-11-23 18:10:11 +0100

    adding analytics package

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import IndexPage from './IndexPage.jsx'
 import Layout from './Layout.jsx'
 import Contact from './Contact/Contact'
 import Blog from './Blogs'
+import { Analytics } from '@vercel/analytics/react';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Route path='/' element={<Layout />} />
       <Route path='/contact' element={<Contact />} />
       <Route path='/blogs' element={<Blog />} />
+      <Analytics />
       </Routes>
     </>
   )
